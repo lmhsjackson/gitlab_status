c74d7e39f4cb90ee32e06c4357e8cfb5185003f7
Jose Piccioni <josepiccioni@gmail.com>
2018-04-13 14:18:53 -0300

    Handle basic Exceptions

--- lib/http_client/http_client.rb
+++ lib/http_client/http_client.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
 require 'rest-client'
+require 'pry'
 
-# Wrapper for httparty
+# Wrapper for rest client
 module HttpClient
   def self.get(url, options = {})
-    RestClient.get(url, options)
+    begin
+      RestClient.get(url, options)
+    rescue RestClient::Exceptions::OpenTimeout => e
+      puts "There was a timeout trying to reach #{url}"
+      e.response
+    rescue SocketError
+      puts 'The network connection failed'
+    end
   end
 end
