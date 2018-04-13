904d273ebcb29220aa4cb5cb83a95b441bdc2e27
Jose Piccioni <josepiccioni@gmail.com>
2018-04-13 13:00:22 -0300

    Add average response and response time methods and specs.

--- lib/gitlab_status.rb
+++ lib/gitlab_status.rb
@@ -1,10 +1,23 @@
 # frozen_string_literal: true
 
 require 'gitlab_status/version'
+require 'http_client/http_client'
+require 'benchmark'
 
 # Here the doc goes
 module GitlabStatus
-  def self.hi_there
-    puts 'Hi there, this is glstatus'
+  def self.average_response(gitlab_page)
+    response_times = []
+    6.times do |n|
+      response_times << response_time(gitlab_page)
+      sleep 10 unless n == 5
+    end
+    response_times.inject { |sum, el| sum + el }.to_f / 6
+  end
+
+  def self.response_time(gitlab_page)
+    Benchmark.realtime do
+      HttpClient.get(gitlab_page)
+    end
   end
 end
--- spec/gitlab_status_spec.rb
+++ spec/gitlab_status_spec.rb
@@ -1,7 +1,34 @@
 # frozen_string_literal: true
 
 RSpec.describe GitlabStatus do
+  TEST_WEB = 'www.some.com'
+  before do
+    allow(subject).to receive(:sleep)
+    allow(HttpClient).to receive(:get)
+  end
+
   it 'has a version number' do
     expect(GitlabStatus::VERSION).not_to be nil
+  end
+
+  describe '#average_response' do
+    it 'gets the web 6 times' do
+      subject.average_response(TEST_WEB)
+      expect(HttpClient).to have_received(:get).with(TEST_WEB)
+                                               .exactly(6).times
+    end
+
+    it 'sleeps 10 seconds 5 times' do
+      subject.average_response(TEST_WEB)
+      expect(subject).to have_received(:sleep).with(10)
+                                              .exactly(5).times
+    end
+  end
+
+  describe '#response_time' do
+    it 'returns a number of seconds' do
+      time = subject.response_time(TEST_WEB)
+      expect(time).to be_kind_of(Float)
+    end
   end
 end
