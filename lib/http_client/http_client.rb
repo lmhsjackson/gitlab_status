298bc8de011f4ae3ea7f9a9b44405fc9e498e202
Jose Piccioni <josepiccioni@gmail.com>
2018-04-13 14:33:35 -0300

    Add exeption handling.

--- lib/http_client/http_client.rb
+++ lib/http_client/http_client.rb
@@ -1,18 +1,15 @@
 # frozen_string_literal: true
 
 require 'rest-client'
-require 'pry'
 
 # Wrapper for rest client
 module HttpClient
   def self.get(url, options = {})
-    begin
-      RestClient.get(url, options)
-    rescue RestClient::Exceptions::OpenTimeout => e
-      puts "There was a timeout trying to reach #{url}"
-      e.response
-    rescue SocketError
-      puts 'The network connection failed'
-    end
+    RestClient.get(url, options)
+  rescue RestClient::Exceptions::OpenTimeout => e
+    puts "There was a timeout trying to reach #{url}"
+    e.response
+  rescue SocketError
+    puts 'The network connection failed'
   end
 end
